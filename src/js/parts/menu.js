24db7e3336ff4e8cd90c039b124e086587c8179f
MyCode07 <erem19992015@mail.ru>
2026-01-13 17:19:51 +0300

    add mobile submenu

--- src/js/parts/menu.js
+++ src/js/parts/menu.js
@@ -5,6 +5,52 @@ const menu = document.querySelector('.menu');
 const searchPopup = document.querySelector('.search');
 
 
+// menu arrow buttom
+const arrow = `<button><svg width="12" height="7" viewBox="0 0 12 7" fill="none"><path d="M1 0.5L6 5.5L11 0.5" stroke-width="1.25"/></svg></button>`;
+
+// add menu summenu opener button
+const submenuList = document.querySelectorAll('.menu ul li');
+if (submenuList.length) {
+    submenuList.forEach(li => {
+        const submenu = li.querySelector('ul');
+        const link = li.querySelector('a');
+
+        if (submenu) {
+            link.insertAdjacentHTML('afterend', arrow);
+            const btn = li.querySelector('button');
+
+            if (btn) {
+                btn.addEventListener('click', function (e) {
+                    toggleMenu(li)
+                })
+            }
+        }
+    })
+
+    function toggleMenu(item) {
+        const menu = item.closest('ul');
+        const menuItems = menu.querySelectorAll('li');
+
+        if (!item.hasAttribute('data-open')) {
+            const openitem = menu.querySelector('li[data-open]');
+            if (openitem) {
+                openitem.removeAttribute('data-open')
+            }
+
+            menuItems.forEach(item => {
+                item.removeAttribute('data-open')
+            })
+
+            item.setAttribute('data-open', 'open')
+        }
+        else {
+            item.removeAttribute('data-open')
+        }
+    }
+}
+
+
+
 document.addEventListener('click', function (e) {
     let targetEl = e.target;
 
